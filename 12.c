a42fc84f177e022345b9d259248f2d866a0b0046
KazimAgha <aghaalikazim@gmail.com>
2017-05-31 22:57:38 +0500

    Update 12.c

--- 12.c
+++ 12.c
@@ -3,16 +3,18 @@
 #include <unistd.h>
 int main (int argc, char *argv[]) {
 pid_t childpid = 0;
-int i, n;
+int a, b;
 if (argc != 2){
 /* check for valid number of command-line arguments */
 fprintf(stderr, "Usage: %s processes\n", argv[0]);
+  /* return the value back */
 return 1;
 }
-n = atoi(argv[1]);
-for (i = 1; i < n; i++)
+b = atoi(argv[1]);
+for (a = 1; a < b; a++)
 if ((childpid = fork()) <= 0)
-break;fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
-i, (long)getpid(), (long)getppid(), (long)childpid);
+break;fprintf(stderr, "a:%d process ID:%ld parent ID:%ld child ID:%ld\n",
+a, (long)getpid(), (long)getppid(), (long)childpid);
 return 0;
 }
+s
