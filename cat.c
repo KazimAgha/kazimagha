fee8faa91e41fca9ec9b320d80c47f6a593dda3f
KazimAgha <aghaalikazim@gmail.com>
2017-05-31 23:52:41 +0500

    Update cat.c

--- cat.c
+++ cat.c
@@ -4,25 +4,25 @@
 
 main(int argc,char *argv[]){
 
-printf("Name: Ahmed Ajmal\nRoll Number: DDP-FA14-BCS-151\nSection: C\n");
+printf("Name: Agha Ali Kazim \nRollno: DDP-FA14-BCS-146\nSection: C\n");
 
 if(argc!=2){
 printf("syntax error\n");
-printf("syntax should be as felllows\n");
+printf("syntax as felllows\n");
 printf("command name file name\n");
 exit(1);
 }
 
-int file,count;
+int file1,count1;
 char buff[22222];
-file=open(argv[1],O_RDONLY);
+file1=open(argv[1],O_RDONLY);
 
-if(file==-1){
-	printf("file do not exist\n");
+if(file1==-1){
+	printf("file not exist\n");
 	exit(1);
 }
 else{
-while((count=read(file,buff,sizeof(buff)))>0){
+while((count1=read(file1,buff,sizeof(buff)))>0){
 	printf("%s",buff);
 }
 exit(0);
